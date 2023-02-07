d0b488380631dc8f02652af15c21df8bfb297f54
FraterPestus <124309399+FraterPestus@users.noreply.github.com>
2023-02-07 14:08:06 +0100

    Update Tricorder.py

--- Tricorder.py
+++ Tricorder.py
@@ -1,41 +1,87 @@
 # Python Code for Tricorder based on Raspberry Pi Zero
-# Program Details:
-# 1.    Kalender mit Datum, Zeit und aktueller (negativer) Sternzeit
-# 1.1   Weltzeit
-# 2.    Wetterstation mit Temperatur, Luftfeuchte und -druck, Verlauf der letzten 48 Stunden
-# 2.1   Anzuzeigende Werte umschaltbar
-# 3.    GPS-basierte Koordinaten mit Kompass
-# 4.    Oberflächentemperatur scannen
-# 5.    Distanzmessung
-# 6.    Laserpointer (auch als Zielhilfe zur Distanzmessung)
-# 7.    Taschenlampe mit Hi-Power-LED
-# 8.    Blinkezeugs zur Demonstration
-# 9.    Akustisches Feedback
-# 10.   3,5" OLED-Screen
-# 11.   Lasst euch überraschen!!!
 
-# Bibliotheken importieren
+#   Program Details:
+# 1.    calendar with date, time and current (negative) sidereal time
+# 1.1   World time
+# 2.    Weather station with temperature, humidity and pressure, history of the last 48 hours
+# 2.1   Shown values switchable
+# 3.    GPS based coordinates with compass
+# 4.    Surface temperature scan
+# 5.    Distance measurement
+# 6.    Laser pointer (also as aiming aid for distance measurement)
+# 7.    Flashlight with Hi-Power-LED (100%, 60%, 30%, SOS at 100%)
+# 8.    Blinking stuff for demonstration 
+# 9.    acoustic feedback
+# 10.   3.5" OLED screen
+# 11.   Be surprised!!!
+
+#   Diagnosis tools:
+#       Uptime (show time the device is switched ON)
+#       show Core temperature
+#       show free space on data storage
+
+
+# Import libraries
 from machine import Pin
 from time import time
 from time import sleep
+#from bme280 import bme280
 
-# LED's definieren
-PWR = Pin(0.pin.OUT)
-Torch = Pin(0.pin.OUT)
-
-
-# Taster definieren
-btnWetter = ()
-btnGPS = ()
-btnTempScan = ()
-btnDistanceScan = ()
-btnPointer = ()
-btnTorch = ()
-reedScreen = ()
+# Describe Pins for LED's
+PWR = Pin(__.pin.OUT)
+Torch = Pin(__.pin.OUT)
 
 
-# Sensoren definieren
+# Describe Pins for Buttons
+btnWeather = (__, Pin.IN, Pin.PULL_DOWN)
+btnGPS = (__, Pin.IN, Pin.PULL_DOWN)
+btnEnvironmentScan = (__, Pin.IN, Pin.PULL_DOWN)
+btnDistanceScan = (__, Pin.IN, Pin.PULL_DOWN)
+btnPointer = (__, Pin.IN, Pin.PULL_DOWN)
+btnTorch = (__, Pin.IN, Pin.PULL_DOWN)
+reedScreen = (__, Pin.IN, Pin.PULL_DOWN)
+
+
+# Describe Pins for sensors
 DHT = Pin(1, Pin.IN)
-GPS = Pin()
-Tem = Pin()
-Dis = Pin()
+GPS = Pin(2, Pin.IN)
+Tem = Pin(3, Pin.IN)
+Dis = Pin(4, Pin.IN)
+
+
+# Create lists for histories
+tempHist = ()
+humidHist = ()
+baroHist = ()
+
+
+
+"""
+Schleife fÃ¼r Hintergrundscan der Umgebung
+while True:
+    temp = bme280_temp()
+    humid = bme280_humidity()
+    baro = bme280_baro()
+
+    TempHist.append(temp)
+    humidHist.append(humid)
+    baroHist.append(baro)
+    if len(tempHist) > 250:
+        tempHist.pop()
+    if len(humidHist) > 250:
+        humidHist.pop()
+    if len(baroHist) > 250:
+        baroHist.pop()
+
+"""
+
+
+
+
+
+
+
+
+
+
+
