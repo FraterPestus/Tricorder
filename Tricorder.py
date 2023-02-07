83f9b589e0331632a3d2dfacc2be316e572066f4
FraterPestus <124309399+FraterPestus@users.noreply.github.com>
2023-02-07 15:18:48 +0100

    Update Tricorder.py

--- Tricorder.py
+++ Tricorder.py
@@ -1,28 +1,28 @@
-# Python Code for Tricorder based on Raspberry Pi Zero
+# Python Code for Tricorder based on Raspberry Pico or Zero, not sure yet
 
-#   Program Details:
-# 1.    calendar with date, time and current (negative) sidereal time
+#   Planned Details for device:
+# 1.    calendar with date, time and current (negative) stardate (Stardate 0 = 01.01.2323)
 # 1.1   World time
 # 1.2   Alarm
 # 2.    Weather station with temperature, humidity and pressure, history of the last 48 hours
 # 2.1   Shown values switchable
-# 3.    GPS based coordinates with compass  (we'll see what we get)
-# 4.    Surface temperature scan            (he'll do his best)
-# 5.    Distance measurement                (not to be 100% reliable)
-# 6.    Heartbeat sensor & Blood Oxymeter   (not suitable for medical investigations!)
-# 7.    Laser pointer (also as aiming aid for distance measurement) (PewPewPew)
-# 8.    Flashlight with Hi-Power-LED (100%, 60%, 30%, SOS at 100%)
+# 3.    GPS based coordinates with compass      (we'll see what we get)
+# 4.    Surface temperature scan                (he'll do his best)
+# 5.    Distance measurement                    (not to be 100% reliable)
+# 6.    Heartbeat sensor & Blood Oxymeter       (not suitable for medical diagnose!)
+# 7.    Laser pointer (also as aiming aid for distance measurement)     (PewPewPew)
+# 8.    Flashlight with Hi-Power-LED (100%, 60%, 30%, SOS at 100%)      (Haaalp!!)
 # 9.    Blinking stuff for demonstration 
 # 10.   acoustic feedback
-# 11.   3.5" OLED screen
+# 11.   3.5" LED screen
 # 12.   Be surprised!!!
 
 #       Silent mode: LED notification, no sounds
 
 #   Diagnosis tools:
 #       Uptime (show time the device is switched ON)
-#       show Core temperature
-#       show free space on data storage
+#       show core temperature
+#       show free space on data storage (if storage will be implemented)
 
 
 # Import libraries
@@ -60,9 +60,10 @@ humidHist = ()
 baroHist = ()
 
 
+# Here the fun starts:
 
 """
-Schleife für Hintergrundscan der Umgebung
+Loop for permanent background environment scan
 while True:
     temp = bme280_temp()
     humid = bme280_humidity()
